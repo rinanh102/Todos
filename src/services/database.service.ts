9e345cee1252113a9667a0ee0f4e3def8ebbe5ff
Dung Nguyen <dungnguyen@chidoanh.com>
2022-06-16 17:33:47 +0700

    connect database

---
+++ src/services/database.service.ts
@@ -0,0 +1,32 @@
+import {injectable} from "inversify";
+import knex, { Knex } from "knex";
+
+@injectable()
+export class DatabaseClient{
+    private _knex: Knex
+
+    constructor(){
+        this._knex = knex({
+            client: 'pg',
+            connection: {
+                host: process.env.DATABASE_HOST, 
+                database: process.env.DATABASE_NAME, 
+                user: process.env.DATABASE_USERNAME,
+                password: process.env.DATABASE_PASSWORD,
+                port: Number(process.env.DATABASE_PORT)
+            },
+            pool: {
+                min: Number(process.env.DATABASE_POOL_MIN),
+                max: Number(process.env.DATABASE_POOL_MAX)
+            },
+            migrations: {
+                tableName: 'KnexMigrations',
+            },
+            acquireConnectionTimeout: 2000
+          });
+    }
+
+    public get knex(): Knex{
+        return this._knex
+    }
+}
